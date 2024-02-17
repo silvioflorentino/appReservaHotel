e1fd5323db3a8804fb963ea33361c1069855b8ac
silvioflorentino <professorsilvioflorentino@yahoo.com.br>
2024-02-16 22:52:36 -0300

    front-end

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-    //
+   public function showHome(){
+        return view("home");
+    }
+
+    public function showFormularioCadastro(Request $request){
+        return view("formularioCadastroCliente");
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[ClienteController::class,'showHome']);
+Route::get('/cadastro-cliente',[ClienteController::class,'showFormularioCadastro']);
